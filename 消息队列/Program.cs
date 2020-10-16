2cc0ce10ccc7f15dcb095595341fde390cc9eb63
宋瑞 <songrui@vip56.cn>
2020-10-16 15:58:11 +0800

    消息队列的基本使用RabbitMQ

---
+++ 消息队列/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace 消息队列
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                factory.UserName = "guest";//用户名
+                factory.Password = "guest";//密码
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare(
+                            queue: "Test",
+                            durable: true,//队列持久化
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null
+                            );
+
+                        string message = GetMessage(args);
+                        var properties = channel.CreateBasicProperties();
+                        properties.Persistent = true;
+
+                        var body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish(
+                            exchange: "",
+                            routingKey: "Test",
+                            basicProperties: properties,//消息持久化
+                            body: body
+                            );//开始传递
+                        Console.WriteLine(" 已发送：{0}", message);
+                    }
+                }
+                args = Console.ReadLine().Split("");
+            }
+        }
+
+        private static string GetMessage(String[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+
+    }
+}
---
+++ 消息队列Receive/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace 消息队列Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "Test",
+                        durable: true,//持久化队列
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                        );
+                    channel.BasicQos(0, 1, false);
+                    var consumer = new EventingBasicConsumer(channel);
+
+
+                    channel.BasicConsume(
+                        queue: "Test",
+                        autoAck: false,
+                        consumer: consumer
+                        );
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+                        Console.WriteLine("已接收：{0}", message);
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    };
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
